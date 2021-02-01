e70077136546482c080b79d870ece74c23fcfd88
Yannick Badoual <yann.badoual@gmail.com>
2021-02-01 18:06:51 +0100

    Optimise stitching

--- main/src/main/kotlin/com/github/badoualy/mobileflow/annotator/Stitcher.kt
+++ main/src/main/kotlin/com/github/badoualy/mobileflow/annotator/Stitcher.kt
@@ -1,7 +1,6 @@
 package com.github.badoualy.mobileflow.annotator
 
 import com.sksamuel.scrimage.ImmutableImage
-import com.sksamuel.scrimage.Position
 import com.sksamuel.scrimage.nio.PngWriter
 import com.sksamuel.scrimage.pixels.Pixel
 import java.io.File
@@ -23,15 +22,43 @@ fun main() {
     println("Stitched in $duration")
 }
 
-private fun List<File>.stitch(startY: Int, endY: Int): ImmutableImage {
-    return map { ImmutableImage.loader().fromFile(it) }
-        .reduce { acc, image ->
-            check(acc.width == image.width) { "Images must have the same width" }
-            val result = checkNotNull(findFirstRowMatch(acc, image, dropFirst = startY, dropLast = image.height - endY))
+fun List<File>.stitch(startY: Int = 0, endY: Int = Integer.MAX_VALUE): ImmutableImage {
+    val chunks = map { ImmutableImage.loader().fromFile(it) }.map { Chunk(it) }
 
-            val bottomChunkSize = image.height - result.second
-            acc.resizeTo(acc.width, result.first + bottomChunkSize, Position.TopLeft)
-                .overlay(image.takeBottom(bottomChunkSize), Position.BottomLeft)
+    // Evaluate chunk bounds
+    chunks.reduce { previousChunk, chunk ->
+        check(previousChunk.image.width == chunk.image.width) { "Images must have the same width" }
+        val result = checkNotNull(
+            findFirstRowMatch(
+                img1 = previousChunk.image,
+                img2 = chunk.image,
+                dropFirst = startY.coerceAtMost(chunk.image.height),
+                dropLast = (chunk.image.height - endY).coerceAtLeast(0)
+            )
+        )
+
+        previousChunk.bounds.bottom = result.first
+        chunk.bounds.top = result.second
+        chunk
+    }
+
+    // Build image
+    val firstChunk = chunks.first()
+    return ImmutableImage.create(firstChunk.image.width, chunks.sumBy { it.height })
+        .apply {
+            awt().createGraphics().apply {
+                chunks.fold(0) { y, chunk ->
+                    drawImage(
+                        chunk.image.awt(),
+
+                        0, y, width, y + chunk.height,
+                        0, chunk.bounds.top, width, chunk.bounds.bottom,
+
+                        null
+                    )
+                    y + chunk.height
+                }
+            }.dispose()
         }
 }
 
@@ -46,7 +73,6 @@ private fun findFirstRowMatch(
 
     val match = img1.rows().dropLast(dropLast).reversed().firstOrNull { it.isIdentical(img2FirstRow) }
     return if (match != null) {
-        println("Found")
         match[0].y to img2FirstRow[0].y
     } else {
         null
@@ -54,3 +80,12 @@ private fun findFirstRowMatch(
 }
 
 private fun Array<out Pixel>.isIdentical(a: Array<out Pixel>): Boolean = all { it.argb == a[it.x].argb }
+
+private data class Chunk(
+    val image: ImmutableImage,
+    val bounds: Bounds = Bounds(left = 0, top = 0, right = image.width, bottom = image.height)
+) {
+    val height get() = bounds.bottom - bounds.top
+}
+
+private data class Bounds(var left: Int = 0, var top: Int = 0, var right: Int = 0, var bottom: Int = 0)
